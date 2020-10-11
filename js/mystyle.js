ff365c4320c04608f3e7bbc2790d2956aa7c1adb
Bladefurey <bladefurey@gmail.com>
2020-10-11 17:29:54 +0630

    template2

--- js/mystyle.js
+++ js/mystyle.js
@@ -21,3 +21,87 @@ $(document).ready(function(){
         $("#button3").show();
     })
 })
+document.getElementById("love1").addEventListener("click",function(){
+    var twocolor="red";
+    var twocolor=document.getElementById("love1").style.color;
+    if (twocolor=="red"){
+        document.getElementById("love1").style.removeProperty("color");
+        document.getElementById("love1").style.color="black";
+        onecolor="black";
+    }
+    else{
+        document.getElementById("love1").style.removeProperty("color");
+        document.getElementById("love1").style.color="red";
+        onecolor="red";
+    }
+})
+document.getElementById("like1").addEventListener("click",function(){
+    var twocolor="blue";
+    var twocolor=document.getElementById("like1").style.color;
+    if (twocolor=="blue"){
+        document.getElementById("like1").style.removeProperty("color");
+        document.getElementById("like1").style.color="black";
+        onecolor="black";
+    }
+    else{
+        document.getElementById("like1").style.removeProperty("color");
+        document.getElementById("like1").style.color="blue";
+        onecolor="blue";
+    }
+})
+document.getElementById("love2").addEventListener("click",function(){
+    var twocolor="red";
+    var twocolor=document.getElementById("love2").style.color;
+    if (twocolor=="red"){
+        document.getElementById("love2").style.removeProperty("color");
+        document.getElementById("love2").style.color="black";
+        onecolor="black";
+    }
+    else{
+        document.getElementById("love2").style.removeProperty("color");
+        document.getElementById("love2").style.color="red";
+        onecolor="red";
+    }
+})
+document.getElementById("like2").addEventListener("click",function(){
+    var twocolor="blue";
+    var twocolor=document.getElementById("like2").style.color;
+    if (twocolor=="blue"){
+        document.getElementById("like2").style.removeProperty("color");
+        document.getElementById("like2").style.color="black";
+        onecolor="black";
+    }
+    else{
+        document.getElementById("like2").style.removeProperty("color");
+        document.getElementById("like2").style.color="blue";
+        onecolor="blue";
+    }
+})
+document.getElementById("love3").addEventListener("click",function(){
+    var twocolor="red";
+    var twocolor=document.getElementById("love3").style.color;
+    if (twocolor=="red"){
+        document.getElementById("love3").style.removeProperty("color");
+        document.getElementById("love3").style.color="black";
+        onecolor="black";
+    }
+    else{
+        document.getElementById("love3").style.removeProperty("color");
+        document.getElementById("love3").style.color="red";
+        onecolor="red";
+    }
+})
+document.getElementById("like3").addEventListener("click",function(){
+    var twocolor="blue";
+    var twocolor=document.getElementById("like3").style.color;
+    if (twocolor=="blue"){
+        document.getElementById("like3").style.removeProperty("color");
+        document.getElementById("like3").style.color="black";
+        onecolor="black";
+    }
+    else{
+        document.getElementById("like3").style.removeProperty("color");
+        document.getElementById("like3").style.color="blue";
+        onecolor="blue";
+    }
+})
